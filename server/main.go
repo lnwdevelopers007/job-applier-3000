d0780a9b4acc762c53cf2fd0d7d35b335bbe53c2
Thanawas Sirilertsathit <146911997+Thanawas-Sirilertsathit@users.noreply.github.com>
2025-08-17 16:17:42 +0700

    Revert "Add another sample data in main.go"
    This reverts commit 7e0c006937902cb12d51ee2269fafc73203e80e6.

--- server/main.go
+++ server/main.go
@@ -22,7 +22,6 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-	{ID: "4", Title: "The Dark Side of the Moon", Artist: "Pink Flyod", Price: 15.99},
 }
 
 // getAlbums responds with the list of all albums as JSON.
