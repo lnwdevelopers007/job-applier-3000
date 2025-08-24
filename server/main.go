8ecfc084de7744059a7eef96ffeaa794c0f10ed6
Thanawas Sirilertsathit <146911997+Thanawas-Sirilertsathit@users.noreply.github.com>
2025-08-24 11:15:50 +0700

    [feat]: Cross origin resource sharing
    - Frontend can connect to backend via API calls

--- server/main.go
+++ server/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/joho/godotenv"
 	"github.com/lnwdevelopers007/job-applier-3000/server/internal/controller"
 )
@@ -13,5 +15,16 @@ func main() {
 		log.Fatal(err)
 	}
 	router := controller.NewRouter()
+	cfg := cors.Config{
+		AllowOrigins: []string{
+			os.Getenv("FRONTEND"),
+		},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
+	router.Use(cors.New(cfg))
 	router.Run(os.Getenv("PORT"))
 }
