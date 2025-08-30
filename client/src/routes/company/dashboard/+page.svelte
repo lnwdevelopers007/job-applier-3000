ff4cfa8becd9b0c3ca59a8e2d9a9d3fc56b264c4
Thanawas Sirilertsathit <146911997+Thanawas-Sirilertsathit@users.noreply.github.com>
2025-08-30 23:23:21 +0700

    [feat]: Dashboard page not include table below
    The upper part of company dashboard page.

---
+++ client/src/routes/company/dashboard/+page.svelte
@@ -0,0 +1,41 @@
+<div class="p-2 bg-gray-100">
+  <h1 class="text-2xl font-bold text-gray-900">
+    Company Dashboard
+  </h1>
+  <p class="my-2 text-base text-gray-600">
+    Welcome back, company HR Team. Here's what’s happening with your recruitment.
+  </p>
+  <div class="mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div class="p-4 bg-white rounded-lg shadow">
+      <h2 class="text-gray-600">Active Jobs</h2>
+      <p class="text-gray-800 text-xl font-bold">8</p>
+      <p class="text-green-500 text-sm">↑ 2 from last month</p>
+    </div>
+
+    <div class="p-4 bg-white rounded-lg shadow">
+      <h2 class="text-gray-600">Total Applicants</h2>
+      <p class="text-gray-800 text-xl font-bold">156</p>
+      <p class="text-gray-500 text-sm">↑ 23 this week</p>
+    </div>
+
+    <div class="p-4 bg-white rounded-lg shadow">
+      <h2 class="text-gray-600">Pending Review</h2>
+      <p class="text-gray-800 text-xl font-bold">18</p>
+      <p class="text-red-500 text-sm">↓ 5 from yesterday</p>
+    </div>
+
+    <div class="p-4 bg-white rounded-lg shadow">
+      <h2 class="text-gray-600">Offers Extended</h2>
+      <p class="text-gray-800 text-xl font-bold">12.</p>
+      <p class="text-red-500 text-sm">↓ 3 this week</p>
+    </div>
+  </div>
+  <div class="mt-8 flex space-x-4">
+    <button class="px-4 py-2 bg-gray-100 text-gray-700 border-1 border-gray-400 font-medium rounded hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:bg-gray-300">
+      Posted Jobs
+    </button>
+    <button class="px-4 py-2 bg-gray-100 text-gray-700 border-1 border-gray-400 font-medium rounded hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:bg-gray-300">
+      Recent Applicants
+    </button>
+  </div>
+</div>
