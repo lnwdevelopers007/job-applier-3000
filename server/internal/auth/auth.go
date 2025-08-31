7c746ef64434956e9b56d361678c56d9bea8f622
Rattapoom Pethputong <celica2548@gmail.com>
2025-08-31 12:30:23 +0700

    feature: upsert user to database.

--- server/internal/auth/auth.go
+++ server/internal/auth/auth.go
@@ -1,18 +1,23 @@
 package auth
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/sessions"
 	"github.com/lnwdevelopers007/job-applier-3000/server/config"
+	"github.com/lnwdevelopers007/job-applier-3000/server/internal/database"
 	"github.com/markbates/goth"
 	"github.com/markbates/goth/gothic"
 	"github.com/markbates/goth/providers/google"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// TODO: add OAuth code here
 func init() {
 	// --- Google Provider Setup ---
 	clientID, _ := config.LoadEnv("CLIENT_ID")
@@ -55,6 +60,37 @@ func addProvider(c *gin.Context) {
 	c.Request.URL.RawQuery = q.Encode()
 }
 
+// upsertUser update or insert user into the database.
+func upsertUser(user goth.User) (*mongo.UpdateResult, error) {
+	db := database.GetDatabase()
+	usersCollection := db.Collection("users")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	filter := bson.M{"_id": user.UserID}
+
+	update := bson.M{
+		"$set": bson.M{
+			"provider":  user.Provider,
+			"email":     user.Email,
+			"name":      user.Name,
+			"avatarURL": user.AvatarURL,
+			"updatedAt": time.Now(),
+		},
+		"$setOnInsert": bson.M{
+			"createdAt": time.Now(),
+		},
+	}
+
+	opts := options.Update().SetUpsert(true)
+
+	res, err := usersCollection.UpdateOne(ctx, filter, update, opts)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
 // OAuthCallback is a callback function after user login.
 func OAuthCallback(c *gin.Context) {
 	addProvider(c)
@@ -68,6 +104,9 @@ func OAuthCallback(c *gin.Context) {
 		return
 	}
 
+	if _, err := upsertUser(user); err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+	}
 	c.JSON(http.StatusOK, user)
 }
 
--- server/internal/schema/user_schema.go
+++ server/internal/schema/user_schema.go
@@ -2,20 +2,14 @@ package schema
 
 import (
 	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
-	ID           primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Provider     string             `bson:"provider" json:"provider"`
-	ProviderID   string             `bson:"provider_id" json:"provider_id"`
-	Email        string             `bson:"email" json:"email"`
-	Name         string             `bson:"name" json:"name"`
-	AvatarURL    string             `bson:"avatar_url" json:"avatar_url"`
-	AccessToken  string             `bson:"access_token,omitempty" json:"-"`
-	RefreshToken string             `bson:"refresh_token,omitempty" json:"-"`
-	ExpiresAt    *time.Time         `bson:"expires_at,omitempty" json:"-"`
-	CreatedAt    time.Time          `bson:"created_at" json:"created_at"`
-	UpdatedAt    time.Time          `bson:"updated_at" json:"updated_at"`
+	ID        string    `bson:"_id"`
+	Provider  string    `bson:"provider"`
+	Email     string    `bson:"email"`
+	Name      string    `bson:"name"`
+	AvatarURL string    `bson:"avatarURL"`
+	UpdatedAt time.Time `bson:"updatedAt"`
+	CreatedAt time.Time `bson:"createdAt"`
 }
