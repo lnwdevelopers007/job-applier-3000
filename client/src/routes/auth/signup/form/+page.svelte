9e75532a90ff4ef305832280c0f110ce2e2abf3a
Pattapon Gowanit <iampattapon@gmail.com>
2025-09-06 21:16:08 +0700

    minor: empty page for signup and login form

---
+++ client/src/routes/auth/signup/form/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { page } from '$app/stores';
+  $: role = $page.url.searchParams.get('role');
+  $: isStudent = role === 'student';
+  $: isCompany = role === 'company';
+</script>
+
+<div>
+  {#if isStudent}
+    <h1>Student Signup Form</h1>
+    <!-- Student signup form -->
+  {:else if isCompany}
+    <h1>Company Signup Form</h1>
+    <!-- Company signup form -->
+  {:else}
+    <h1>Please select a role</h1>
+    <a href="/auth/signup">Go back</a>
+  {/if}
+</div>
