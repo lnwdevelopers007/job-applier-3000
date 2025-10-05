3448947e391c65b7a613b8dc2469ce8e2a342b35
Rattapoom Pethputong <celica2548@gmail.com>
2025-10-05 16:16:07 +0700

    minor: fix CompanyInfo to match frontend's

--- server/internal/schema/company_info.go
+++ server/internal/schema/company_info.go
@@ -3,9 +3,11 @@ package schema
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type CompanyInfo struct {
-	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	Name        string             `bson:"name" json:"name" binding:"required"`
-	UserID      primitive.ObjectID `bson:"userID" json:"userID" binding:"required"`
-	AboutUs     string             `bson:"aboutUs" json:"aboutUs" binding:"required"`
-	CompanyType string             `bson:"companyType" json:"companyType" binding:"required"`
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	UserID   primitive.ObjectID `bson:"userID" json:"userID" binding:"required"`
+	Name     string             `bson:"name" json:"name" binding:"required"`
+	AboutUs  string             `bson:"aboutUs" json:"aboutUs" binding:"required"`
+	Industry string             `bson:"industry" json:"industry" binding:"required"`
+	Size     string             `bson:"size" json:"size" binding:"required"`
+	Website  string             `bson:"website" json:"website" binding:"required"`
 }
