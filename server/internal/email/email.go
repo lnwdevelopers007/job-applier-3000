8566b6650a0c268707e4f55ad741744f11bada39
Rattapoom Pethputong <celica2548@gmail.com>
2025-10-11 17:18:31 +0700

    feature: send email via smtp

--- server/internal/email/email.go
+++ server/internal/email/email.go
@@ -1,3 +1,32 @@
 package email
 
-// TODO: Add email-related codes here.
+import (
+	"log"
+	"net/smtp"
+
+	"github.com/lnwdevelopers007/job-applier-3000/server/config"
+)
+
+func Send(target, subject, body string) {
+
+	from := config.LoadEnv("EMAIL")
+	pass := config.LoadEnv("EMAIL_PASSWORD")
+
+	msg := []byte(
+		"Subject: " + subject + "\r\n" +
+			"\r\n" +
+			body + "\r\n",
+	)
+
+	smtpHost := config.LoadEnv("EMAIL_PROVIDER")
+	smtpPort := config.LoadEnv("EMAIL_PROVIDER_PORT")
+
+	auth := smtp.PlainAuth("", from, pass, smtpHost)
+
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, []string{target}, msg)
+	if err != nil {
+		log.Fatal("Failed to send email:", err)
+	}
+
+	log.Println("Email sent successfully!")
+}
