972d071cc5715c892735307768e320ad93c1f07d
Rattapoom Pethputong <celica2548@gmail.com>
2025-10-11 21:21:37 +0700

    feature: send email when job seeker applied for a job (UC-S04)

--- server/internal/controller/utils.go
+++ server/internal/controller/utils.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/lnwdevelopers007/job-applier-3000/server/internal/database"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -40,6 +41,21 @@ func findAll[Schema any](
 	var result []Schema
 	if err := cursor.All(ctx, &result); err != nil {
 		return nil, err
+	}
+	return result, nil
+}
+
+func findOne[Schema any](
+	ctx context.Context,
+	collectionName string,
+	objID primitive.ObjectID,
+) (Schema, error) {
+	collection := database.GetDatabase().Collection(collectionName)
+	var result Schema
+	err := collection.FindOne(ctx, bson.M{"_id": objID}).Decode(&result)
+	if err != nil {
+		var thing Schema
+		return thing, err
 	}
 	return result, nil
 }
