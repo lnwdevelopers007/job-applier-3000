b4c3adc9df25fd73c9c67e9f4f500a15a5df40e1
Rattapoom Pethputong <celica2548@gmail.com>
2025-11-02 11:37:40 +0700

    refactor: use apiFetch for fetcher.ts

--- client/src/lib/utils/fetcher.ts
+++ client/src/lib/utils/fetcher.ts
@@ -1,12 +1,12 @@
+import { apiFetch } from "./api";
+
 export const DEFAULT_COMPANY_LOGO =
   'https://images.unsplash.com/photo-1534237710431-e2fc698436d0?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YnVpbGRpbmd8ZW58MHx8MHx8fDA%3D';
 
 export const DEFAULT_COMPANY_NAME = 'Unknown Company';
 
-async function fetchThing(endpoint: string, params: string = "") {
-  const res = await fetch(`/${endpoint}/${params}`, {
-    credentials: 'include'
-  });
+async function fetchData(endpoint: string, params: string = "") {
+  const res = await apiFetch(`/${endpoint}/${params}`)
   if (!res.ok) throw new Error(`Failed to fetch ${endpoint} details (Status: ${res.status})`);
   const data = await res.json();
   return data;
@@ -14,15 +14,15 @@ async function fetchThing(endpoint: string, params: string = "") {
 
 
 export async function fetchJob(jobID: string) {
-  return fetchThing('jobs', jobID)
+  return fetchData('jobs', jobID)
 }
 
 export async function fetchUser(userID: string) {
-  return fetchThing('users', userID)
+  return fetchData('users', userID)
 }
 
 export async function fetchUsers(params?: string) {
-  return fetchThing('users')
+  return fetchData('users', params)
 }
 
 export async function fetchCompanyNameLogo(companyID: any) {
